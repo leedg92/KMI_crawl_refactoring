851532fe301598726cdfa36fdc87619cbffcc372
hongs <ghdrmsg@gmail.com>
2024-12-04 11:28:00 +0900

    investing 크롤링 방법 수정

--- dags/investing_bdi_index.py
+++ dags/investing_bdi_index.py
@@ -75,139 +75,91 @@ def upsert_to_dataframe(result_dataframe, table_name, val_list):
     finally:
         conn.close()
 
-
-def wait_for_csv_and_read(download_path, timeout=60):
-    """
-    지정된 다운로드 경로에서 CSV 파일을 기다린 후 읽어서 DataFrame으로 반환.
-    :param download_path: 다운로드 폴더 경로
-    :param timeout: 다운로드가 완료되기를 기다리는 최대 시간 (초 단위)
-    :return: DataFrame 또는 None (타임아웃 시)
-    """
-    end_time = time.time() + timeout
-    while time.time() < end_time:
-        # 다운로드 폴더에서 CSV 파일 찾기
-        for file_name in os.listdir(download_path):
-            if file_name.endswith('.csv'):
-                csv_file_path = os.path.join(download_path, file_name)
-                print(f'다운로드 파일 발견: {csv_file_path}')
-                try:
-                    return file_name
-                except Exception as e:
-                    print(f'[error] CSV 파일 읽기 중 오류 발생: {e}')
-                    return None
-        time.sleep(1) 
-    print(f'[warning] 타임아웃: {download_path}에서 CSV 파일을 찾을 수 없습니다.')
-    return None
-def random_sleep():
-    time.sleep(random.uniform(1, 3))
-
-
-def preprocessing_data(term, origin_df):
-    df = origin_df.copy()
-
-    columns_mapping = INVESTING_COLS_MAPPING
-    df.rename(columns=columns_mapping, inplace=True)
-    drop_col_name = ['VOL']
-    df.drop(columns=drop_col_name, axis=1, inplace=True)
-
-    df['PRICE'] = df['PRICE'].str.replace(',', '').astype(float)
-    df['OPEN'] = df['OPEN'].str.replace(',', '').astype(float)
-    df['HIGH'] = df['HIGH'].str.replace(',', '').astype(float)
-    df['LOW'] = df['LOW'].str.replace(',', '').astype(float)
-    df['CHANGE'] = df['CHANGE'].str.replace(',', '').str.rstrip('%').astype(float)
-
-    datetime_col_name = 'DATE'
-    df['YEAR'] = pd.to_datetime(df[datetime_col_name]).dt.year.astype(str).str.zfill(4)
-    df['MONTH'] = pd.to_datetime(df[datetime_col_name]).dt.month.astype(str).str.zfill(2)
-    if term == 'W':
-        df['DAY'] = pd.to_datetime(df[datetime_col_name]).dt.day.astype(str).str.zfill(2)
-        df['WEEK'] = pd.to_datetime(df[datetime_col_name]).apply(calculate_week_starting_sunday)
-        
-    elif term == 'D':
-        df['DAY'] = pd.to_datetime(df[datetime_col_name]).dt.day.astype(str).str.zfill(2)
-        df.drop(columns=datetime_col_name, axis=1, inplace=True)
-        df = df.replace({np.nan: None})        
-        
+def calculate_week_starting_sunday(date):
+    # 일요일을 시작으로 주차 계산
+    sunday = date - pd.Timedelta(days=date.weekday() + 1) if date.weekday() != 6 else date
+    return sunday.isocalendar()[1]
     
-    df.drop(columns=datetime_col_name, axis=1, inplace=True)
-    df = df.replace({np.nan: None})
-        
-    return df
-
-
-    
-def investing_file_download(time_frame):
+def investing_file_download(time_frame, term):
     
     # Set selenium
     opts = set_selenium_options()
-    opts.add_argument("--disable-dev-shm-usage")  # 공유 메모리 비활성화
-    opts.add_argument("--disable-devtools")  # DevTools 비활성화
-    opts.add_argument("--remote-debugging-port=0")  # DevTools 디버깅 포트 비활성화
     browser = set_webdriver_browser(opts, INVESTING_DOWNLOAD_PATH)
 
-    # URL Access    
+    # URL Access
     try:
         browser.get(INVESTING_URL)
-        browser.implicitly_wait(10)
+        browser.implicitly_wait(60)
+        print("OPEN URL")
         time.sleep(0.5)
-                  
-        # 다운로드 버튼 클릭        
-        download_btn_element = WebDriverWait(browser, 60).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/div/span[2]'))
-        )
-        browser.execute_script("arguments[0].click();", download_btn_element)
-        
-        # login 처리         
-        sign_in_btn_element = WebDriverWait(browser, 60).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[4]/div/div/form/p[2]/button')))
-        browser.execute_script("arguments[0].click();", sign_in_btn_element)
-        
-        print(f'[Login] Input User ID ~~~')
-        user_id_input_element = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[4]/div/div/form/div[3]/input')))
-        user_id_input_element.send_keys(INVESTING_USER_ID)
-        random_sleep()
-        # Login - Input USER PW
-        print(f'[Login] Input User PW ~~~')
-        user_pw_input_element = browser.find_element(By.XPATH, '/html/body/div[4]/div/div/form/div[5]/input')
-        user_pw_input_element.send_keys(INVESTING_USER_PW)
-        random_sleep()
-        
-        login_finish_btn_element = WebDriverWait(browser, 20).until(
-            EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div/form/button'))
-        )
-        login_finish_btn_element.click()
-        print(f'[Login] Success Login.')
 
-       
         # time frame 선택
         time_frame_element = browser.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/div[2]/span')         
         browser.execute_script("arguments[0].scrollIntoView(true);", time_frame_element)
         browser.execute_script("arguments[0].click();", time_frame_element)
         
-        random_sleep()
+        time.sleep(1)
         print('time frame 선택')
         term_select_btn_element = browser.find_element(By.XPATH, time_frame) 
         browser.execute_script("arguments[0].click();", term_select_btn_element)
         print('time frame 주기 선택')
-        random_sleep()
+        time.sleep(1)
+
+
+        try:
+            text = browser.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]/div[3]/table')
+        except Exception as e:
+            print("Element not found XPath (table) . Error:", e)
+            browser.quit()
+            raise e
+            
+        raw_data = text.text
+
+        lines = raw_data.split("\n")    
+        header = ["Date", "Price", "Open", "High", "Low", "Change"]
+
+        rows = []
+        for line in lines[1:]:
+            parts = line.split()
+            if len(parts) >= len(header):  
+                date = " ".join(parts[:3])  
+                row = [date] + parts[3:len(header)+3]  
+                rows.append(row)
+
+        rows = [
+            row if len(row) == len(header) else row + ["-"] * (len(header) - len(row))
+            for row in rows
+        ]
+
+        df = pd.DataFrame(rows, columns=header)
+
+        # Map month names to numbers
+        month_map = {
+            "Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06",
+            "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"
+        }
+
+        # Extract Year, Month, Day from Date
+        df['YEAR'] = df['Date'].apply(lambda x: x.split()[-1])  
+        df['MONTH'] = df['Date'].apply(lambda x: month_map[x.split()[0]])  
+        if(term != 'M'):
+            df['DAY'] = df['Date'].apply(lambda x: x.split()[1].replace(",", ""))
+        if(term == 'W'):
+            df['WEEK'] = pd.to_datetime(df['Date']).apply(calculate_week_starting_sunday)
         
-        # 다운로드 버튼 클릭        
-        download_btn_element = WebDriverWait(browser, 60).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/div/div[1]/span'))
-        )
-        browser.execute_script("arguments[0].click();", download_btn_element)
+        df.columns = [col.upper() for col in df.columns]
+        df['CHANGE'] = df['CHANGE'].replace('%', '', regex=True)
+        df.drop(columns=['DATE'], inplace=True)
+
+
+        print(df)
+
+        browser.quit()
         
-        print('다운로드 버튼 클릭')
-        
-        new_files = wait_for_csv_and_read(INVESTING_DOWNLOAD_PATH)
-        
-        
-        return new_files       
-        
-        
+        return df
     except Exception as e:
-        print(e)
-        browser.close()
-        sys.exit()
+        print(f"An error occurred: {e}")
+        browser.quit()
     
     
 ##. [일간] Baltic Dry Index Historical Data (BADI) 데이터 (테이블 명 : fct_investing_bdi_index_daily)
@@ -216,25 +168,14 @@ def func1():
     
     time_frame_daliy = '/html/body/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/div[2]/div/div[1]/span'
     term = 'D'
-    file_names = investing_file_download(time_frame_daliy)
-    print(file_names)
+    result_df = investing_file_download(time_frame_daliy, term)
     
-    full_file_name = INVESTING_DOWNLOAD_PATH + file_names
-    done_file_name = INVESTING_DESTINATION_PATH + term +'_' + get_year_month_day() + '_' + file_name
-        
-    origin_df = pd.read_csv(full_file_name, header=0, dtype=str)
-    result_df = preprocessing_data(term , origin_df)
-
     val_list = []
     for index, result in result_df.iterrows():
         val_list.append(result.tolist())
     
     def_table_name = 'fct_investing_bdi_index_daily'
     upsert_to_dataframe(result_df, def_table_name, val_list)
-    
-    # 데이터 추출이 완료된 파일, done 으로 이동
-    shutil.move(full_file_name, done_file_name)
-    
     
     print('--' * 10, '(end) investing_bdi_index_collector (daily)', '--' * 10)
     
@@ -244,9 +185,14 @@ def func2():
     
     time_frame_weekly = '/html/body/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/div[2]/div/div[2]/span'    
     term = 'W'
-    download_file = investing_file_download(time_frame_weekly)
+    result_df = investing_file_download(time_frame_weekly, term)
     
+    val_list = []
+    for index, result in result_df.iterrows():
+        val_list.append(result.tolist())
     
+    def_table_name = 'fct_investing_bdi_index_weekly'
+    upsert_to_dataframe(result_df, def_table_name, val_list)
     
     print('--' * 10, '(end) investing_bdi_index_collector (weekly)', '--' * 10)
     
@@ -256,7 +202,14 @@ def func3():
     
     time_frame_monthly = '/html/body/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/div[2]/div/div[3]/span'    
     term = 'M'
-    download_file = investing_file_download(time_frame_monthly)
+    result_df = investing_file_download(time_frame_monthly, term)
+    
+    val_list = []
+    for index, result in result_df.iterrows():
+        val_list.append(result.tolist())
+    
+    def_table_name = 'fct_investing_bdi_index_monthly'
+    upsert_to_dataframe(result_df, def_table_name, val_list)
     
     print('--' * 10, '(end) investing_bdi_index_collector (monthly)', '--' * 10)
     
@@ -271,17 +224,17 @@ investing_bdi_index_daily = PythonOperator(
     dag = init_dag
 )
 
-# investing_bdi_index_weekly = PythonOperator(
-#     task_id = 'investing_bdi_index_weekly',
-#     python_callable = func2,
-#     dag = init_dag
-# )
+investing_bdi_index_weekly = PythonOperator(
+    task_id = 'investing_bdi_index_weekly',
+    python_callable = func2,
+    dag = init_dag
+)
 
-# investing_bdi_index_monthly = PythonOperator(
-#     task_id = 'investing_bdi_index_monthly',
-#     python_callable = func3,
-#     dag = init_dag
-# )
+investing_bdi_index_monthly = PythonOperator(
+    task_id = 'investing_bdi_index_monthly',
+    python_callable = func3,
+    dag = init_dag
+)
 
 
-task_start >> investing_bdi_index_daily >> task_end
+task_start >> investing_bdi_index_daily >> investing_bdi_index_weekly >> investing_bdi_index_monthly >> task_end
