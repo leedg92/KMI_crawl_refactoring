4690c06276357e30fcbdb70c151eeb8fbaf9975b
hongs <ghdrmsg@gmail.com>
2024-12-23 15:05:51 +0900

    [kcia] 스케쥴 설정

--- dags/kcia_oil_product.py
+++ dags/kcia_oil_product.py
@@ -24,13 +24,14 @@ import pymysql
 ######################
 init_args = {
     'owner' : OWNER_NAME,
-    'start_date' : datetime.datetime(2024, 11, 11)
+    'start_date' : datetime.datetime(2024, 11, 11),
+    'retries': 1
 }
 init_dag = DAG(
     dag_id = 'kcia_oil_product_collector',
     default_args = init_args,
     # schedule_interval = '@once'
-    schedule_interval = '0 1 * * *',
+    schedule_interval = '0 2 1 1 *',
     catchup=False
 )
 task_start = DummyOperator(task_id='start', dag=init_dag)
